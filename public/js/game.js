32bfd1b418324df70d1eb1bcb73883b52e5aa13e
Jörg <mail@joerg-heilmann.de>
2024-12-27 20:43:34 +0100

    Fix issue when loading before the game started

--- public/js/game.js
+++ public/js/game.js
@@ -99,7 +99,6 @@ document.addEventListener('DOMContentLoaded', function () {
         const gameSessionLevelElement = document.getElementById('game-session-level');
         gameSessionLevelElement.textContent = levelTitle;
     }
-    renderGameSessionLevel(gameSession.getCurrentLevel().title);
     gameSession.subscribe('currentLevelId', () => {
         renderGameSessionLevel(gameSession.getCurrentLevel().title);
     });
